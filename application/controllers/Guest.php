c6405f9274ae19df64a648441c15fa26479b4f73
sayooj <sayoojprabhakaran007@gmail.com>
2018-02-04 10:40:03 +0530

    Undefined variable: enquieries_id
    Filename: controllers/Guest.php

--- application/controllers/Guest.php
+++ application/controllers/Guest.php
@@ -177,7 +177,7 @@ class Guest extends CI_Controller
 			if($this->input->post('number_of_days') < $this->input->post('number_of_nights'))
 			{
 				$this->error('<p class="error">No of days should be greater than nights</p>');
-				redirect('index.php/Guest/add_guest_enquiry_view/'.$enquieries_id.'#number_of_nights');
+				redirect('$_SERVER['HTTP_REFERER'].'#number_of_nights');
 			}
 
 
